00e4636fac942dab876a721fbd3860e70d5c1b9a
Carl Gay <carlgay@gmail.com>
2021-03-13 19:59:38 +0000

    Minor refactor of lsp-dylan-start

--- setup.el
+++ setup.el
@@ -7,16 +7,20 @@
 (add-to-list 'lsp-language-id-configuration '(dylan-mode . "dylan"))
 
 (defun lsp-dylan-start ()
-  (let* ((dylan-root (getenv "DYLAN"))
-         (lsp-dylan-relative-path "_build/bin/lsp-dylan")
-         (lsp-dylan-full-path
-          (if dylan-root
-              ;; Assume using dylan-tool if $DYLAN is set.
-              ;; Also assume $DYLAN/workspaces/lsp as the workspace directory.
-              (format "%s/workspaces/lsp/_build/bin/lsp-dylan" dylan-root)
-            ;; Otherwise assume using git submodules in the lsp-dylan directory.
-            (expand-file-name lsp-dylan-relative-path load-file-name)))
-         (server (list lsp-dylan-full-path "--debug")))
+  (let* ((relative-path "_build/bin/lsp-dylan")
+         (chosen-path
+          (cond ((file-exists-p relative-path)
+                 ;; If current directory has a _build directory, prefer that.
+                 relative-path)
+                ((getenv "DYLAN")
+                 ;; Assume using dylan-tool and $DYLAN/workspaces/lsp as the
+                 ;; workspace directory.
+                 ;; TODO(cgay): needs better solution. this works for me.
+                 (concat (getenv "DYLAN") "/workspaces/lsp/" relative-path))
+                (t
+                 (error "Couldn't find the lsp-dylan executable"))))
+         (full-path (expand-file-name chosen-path (file-name-directory load-file-name)))
+         (server (list full-path "--debug")))
     (lsp-register-client
      (make-lsp-client :new-connection (lsp-stdio-connection server)
                       :major-modes '(dylan-mode)
