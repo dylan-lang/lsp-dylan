a841141bfca8bfaa2117d97a2892b86032ddc4fd
Carl Gay <carlgay@gmail.com>
2024-03-10 14:55:28 -0400

    lsp-dylan.el: evaluate lsp-dylan--command at run-time
    Recommended so users can customize it at run-time if desired.

--- lsp-dylan.el
+++ lsp-dylan.el
@@ -79,11 +79,14 @@
 (defun lsp-dylan--start ()
   "Do whatever we need to set up and register with emacs-lsp"
   (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection 'lsp-dylan--command)
-		    :environment-fn 'lsp-dylan--environment
-		    :major-modes '(dylan-mode)
-		    :initialized-fn 'lsp-dylan--initialized
-		    :server-id 'lsp-dylan)))
+   (make-lsp-client
+    ;; Note we wrap lsp-dylan--command in lambda so users can customize it at
+    ;; run time.
+    :new-connection (lsp-stdio-connection 'lsp-dylan--command)
+    :environment-fn 'lsp-dylan--environment
+    :major-modes '(dylan-mode)
+    :initialized-fn 'lsp-dylan--initialized
+    :server-id 'lsp-dylan)))
 
 (lsp-consistency-check lsp-dylan)
 (lsp-dylan--start)
